59d403a3c99e4ba9dc4e4e9ed21291ec9559a1fe
vanberge <vanberge@gmail.com>
2018-10-16 10:16:10 -0400

    updating message from main app

--- demo-app.py
+++ demo-app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<center>Hello, world</center>'
+    return '<center>Hello world!</center>'
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
